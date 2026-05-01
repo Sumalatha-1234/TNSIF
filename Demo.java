7e1b703c753de44098c39cb33ae61b2fb96f2a68
Sumalatha-1234 <sumalatha2032003@gmail.com>
2026-05-01 13:06:53 +0530

    this is my --first commit

---
+++ Demo.java
@@ -0,0 +1,18 @@
+package com.tnsif.basicPrograms;
+
+public class Demo {
+
+	int _number=9;
+	
+	int _NUMBER=10;
+	
+	int $huu=9;
+	
+	//int short=20;
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("wELCOME TO JAVA...");
+	}
+}
